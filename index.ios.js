8a96f7c213da9a036093e5cb99c15465653d0dd3
Scott Crespo <screspo@highcornergroup.com>
2015-08-17 00:55:36 -0400

    testing basic navigation

--- index.ios.js
+++ index.ios.js
@@ -5,31 +5,78 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  Navigator
 } = React;
 
-var ReactNativeDemo = React.createClass({
-  render: function() {
+class ReactNativeDemo extends React.Component {
+  render() {
+    return (
+      <Navigator
+        initialRoute={{name: 'Welcome', component: Welcome}}
+        configureScene={() => {
+            return Navigator.SceneConfigs.FloatFromRight;
+        }}
+        renderScene={(route, navigator) => {
+            if (route.component) {
+                return React.createElement(route.component, { navigator });
+            }
+        }}
+      />
+    );
+  }
+}
+
+class Welcome extends React.Component {
+
+  onPressList() {
+      this.props.navigator.push({
+          name: 'TheList',
+          component: TheList
+      });
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome Screen
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text onPress={this.onPressList.bind(this)}>
+          Go to List!
         </Text>
       </View>
-    );
+    )
   }
-});
+}
+
+class TheList extends React.Component {
+
+  onPressWelcome() {
+      this.props.navigator.push({
+          name: 'Welcome',
+          component: Welcome
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          The List
+        </Text>
+        <Text onPress={this.onPressWelcome.bind(this)}>
+          Go to Welcome!
+        </Text>
+      </View>
+    )
+  }
+}
 
 var styles = StyleSheet.create({
   container: {
