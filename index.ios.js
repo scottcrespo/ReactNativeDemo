a2d316d46d16ad63f3ee773665279148fce70a0f
Scott Crespo <screspo@highcornergroup.com>
2015-08-19 10:40:28 -0400

    implmemented example 1

--- index.ios.js
+++ index.ios.js
@@ -17,64 +17,38 @@ var {
 class ReactNativeDemo extends React.Component {
   render() {
     return (
-      <Navigator
-        initialRoute={{name: 'Welcome', component: Welcome}}
-        configureScene={() => {
-            return Navigator.SceneConfigs.FloatFromRight;
-        }}
-        renderScene={(route, navigator) => {
-            if (route.component) {
-                return React.createElement(route.component, { navigator });
-            }
-        }}
-      />
+      <View style={styles.container} >
+        <Text style={styles.lrgtxt}>
+          Master Text
+        </Text>
+        <Child1 />
+      </View>
     );
   }
 }
 
-class Welcome extends React.Component {
-
-  onPressList() {
-      this.props.navigator.push({
-          name: 'TheList',
-          component: TheList
-      });
-  }
-
+class Child1 extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome Screen
+    return(
+      <View>
+        <Text style={styles.lrgtxt}>
+          Child1 Text
         </Text>
-        <Text onPress={this.onPressList.bind(this)}>
-          Go to List!
-        </Text>
+        <Child2 />
       </View>
-    )
+    );
   }
 }
 
-class TheList extends React.Component {
-
-  onPressWelcome() {
-      this.props.navigator.push({
-          name: 'Welcome',
-          component: Welcome
-      });
-  }
-
+class Child2 extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          The List
-        </Text>
-        <Text onPress={this.onPressWelcome.bind(this)}>
-          Go to Welcome!
+    return(
+      <View>
+        <Text style={styles.lrgtxt}>
+          Child2 Text
         </Text>
       </View>
-    )
+    );
   }
 }
 
@@ -85,7 +59,7 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  lrgtxt: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
